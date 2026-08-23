fe8a761084a1739cccd0fab521f55df1cccb616b
Gabriel Zambo <gabrieltzmb@gmail.com>
2026-08-22 21:41:06 -0300

    feat:Cria botão de  avaliação do atendimento

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 const Contato = ({id, nome, telefone, email}) => {
+    const [avaliacao, setAvaliacao] = useState(0);
   return (
     <div>
       <h2>NOME: {nome}</h2>
       <p>ID: {id}</p>
       <p>Telefone:{telefone}</p>
-      <p>E-mail:{telefone}</p>
+      <p>E-mail:{email}</p>
+     <div>
+    <p>Avalie o atendimento:</p>
+
+    <button onClick={() => setAvaliacao(1)}>⭐ 1</button>
+    <button onClick={() => setAvaliacao(2)}>⭐ 2</button>
+    <button onClick={() => setAvaliacao(3)}>⭐ 3</button>
+    <button onClick={() => setAvaliacao(4)}>⭐ 4</button>
+    <button onClick={() => setAvaliacao(5)}>⭐ 5</button>
+
+    <p>Avaliação: {avaliacao} ⭐</p>
+</div>
     </div>
   )
 }
